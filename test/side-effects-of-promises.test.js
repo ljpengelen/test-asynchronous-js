25833c45e6e2ec9ed3c4867ecf973d91c4ecf279
Luc Engelen <luc@kabisa.nl>
2018-02-17 14:18:32 +0100

    Demonstrate testing side-effects

---
+++ test/side-effects-of-promises.test.js
@@ -0,0 +1,38 @@
+const expect = require("chai").expect;
+
+let message = "Unresolved";
+Promise.resolve("Resolved").then(r => message = r);
+
+class Delay {
+  constructor() {
+    this.message = "Unresolved";
+  }
+
+  generateMessage() {
+    Promise.resolve("Resolved").then(r => this.message = r);
+  }
+}
+
+describe("Side effects of promises", function() {
+  it("passes, because promise has already been resolved", function() {
+    expect(message).to.equal("Resolved");
+  });
+
+  it("fails, because promise doesn't resolve immediately", function() {
+    const delay = new Delay();
+    expect(delay.message).to.equal("Unresolved");
+    delay.generateMessage();
+    expect(delay.message).to.eql("Resolved");
+  });
+
+  it("passes, because promise did resolve", function(done) {
+    const delay = new Delay();
+    expect(delay.message).to.equal("Unresolved");
+    delay.generateMessage();
+    setTimeout(() => {
+      expect(delay.message).to.eql("Resolved");
+      done()
+    });
+  });
+
+});
